53b225fc7acedd567641f534381ab10cd6393586
Andre Oliveira <andreo@debian-BULLSEYE-live-builder-AMD64>
2022-09-29 13:44:09 -0300

    Versão 1 - Atualizada

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main(){
 
-  int n = 2; // Número de fibonacci
+  int n = 4; // Número de fibonacci
   int fib[n];
 
   if (n == 0){
@@ -20,13 +20,16 @@ int main(){
 
   else{
 
+    fib[0] = 0;
+    fib[1] = 1;
+
     for(int i = 2; i <= n; i++){
 
-      fib[i] = fib[i-1]*fib[i-2];
+      fib[i] = fib[i-1]+fib[i-2];
      }
   }
 
-  printf("Número de Fibonnaci para n = %d é:%d\n ",n,fib[n]);
+  printf("Número de Fibonnaci para n = %d é: %d\n ",n,fib[n]);
 
   return 0;
 }
